3f8fe2ac1056588238889844cf87f2bac5a239f3
Mythili86 <134497482+Mythili86@users.noreply.github.com>
2023-06-29 21:21:46 +0530

    Update detect_faces.py

--- detect_faces.py
+++ detect_faces.py
@@ -6,11 +6,11 @@ import cv2
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
-	help="C:\Users\HP\Downloads")
+	help="path to input image")
 ap.add_argument("-p", "--prototxt", required=True,
-	help="C:\Users\HP\Downloads")
+	help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True,
-	help="C:\Users\HP\Downloads")
+	help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
@@ -21,7 +21,7 @@ net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 
 
-image = cv2.imread(args["image.py"])
+image = cv2.imread(args["image"])
 (h, w) = image.shape[:2]
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
 	(300, 300), (104.0, 177.0, 123.0))
